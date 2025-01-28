f75918fc7e33fd227d844ffd7a72dcb59b043ac2
tkkap04 <mono.takaki@gmail.com>
2025-01-29 01:32:06 +0900

    genresテーブルの修正

--- src/database/migrations/2024_07_21_005229_create_genres_table.php
+++ src/database/migrations/2024_07_21_005229_create_genres_table.php
@@ -16,7 +16,7 @@ class CreateGenresTable extends Migration
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image_url');
+            $table->string('image_url')->nullable();
             $table->timestamps();
         });
     }
